b2910095137685dedd4b38bb2d5f08a1619dc215
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-28 00:25:52 -0500

    *ngIf => Directiva Condicional.

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'videojuegos',
-  template: `
+    selector: 'videojuegos',
+    template: `
     <h2> Componente de {{nombre}} </h2>
-    <h3> El mejor juego es : {{mejor_juego}}</h3>
+    
+    <h3 [style.background]='color' *ngIf="mostrar_retro != true"> 
+        El mejor juego es : {{mejor_juego}}
+    </h3>
+
+    <h3 [style.background]='color' *ngIf="mostrar_retro"> 
+        El mejor juego es : {{mejor_juego_retro}}
+    </h3>
   `
 })
 
 export class VideojuegosComponent {
-  nombre = 'Videojuegos 2018';
-  mejor_juego = 'GTAV';
+    nombre = 'Videojuegos 2018';
+    mejor_juego = 'GTAV';
+    mejor_juego_retro = 'Super mario 64';
+    mostrar_retro = false;
+    color = this.mostrar_retro? "red" : "yellow";
 }
