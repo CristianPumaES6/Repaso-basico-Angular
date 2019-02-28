49e13be95a946979500f7e6756b4a8c8e75f3a44
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-27 23:58:59 -0500

    Agregamos una nueva propiedad a nuestro componente VideoJuegos.

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'videojuegos',
   template: `
     <h2> Componente de {{nombre}} </h2>
+    <h3> El mejor juego es : {{mejor_juego}}</h3>
   `
 })
 
 export class VideojuegosComponent {
-  nombre = 'Videojuegos 2018'
+  nombre = 'Videojuegos 2018';
+  mejor_juego = 'GTAV';
 }
