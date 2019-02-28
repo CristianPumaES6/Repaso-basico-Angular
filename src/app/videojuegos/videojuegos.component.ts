2d896e90f1fdfda3cceb42aa225a4f7868c6b54d
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-28 01:02:57 -0500

    Se creo un constructor y se edito la class Videojeugos.

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -2,21 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'videojuegos',
-    templateUrl:'./videojuegos.component.html'
+    templateUrl: './videojuegos.component.html'
 })
 
 export class VideojuegosComponent {
-    nombre = 'Videojuegos 2018';
-    mejor_juego = 'GTAV';
-    mejor_juego_retro = 'Super mario 64';
-    mostrar_retro = false;
-    color = this.mostrar_retro? "red" : "yellow";
+    nombre: string;
+    mejor_juego: string;
+    mejor_juego_retro: string;
+    mostrar_retro: boolean;
+    color: string;
+    year: number;
+    videojuegos: Array<string>;
 
-    videojuegos = [
-        'los simpson hit and rin',
-        'assins creed',
-        'Dragon ball Z',
-        'Teken Tag',
-        'Naruto Duo'
-    ]
+    constructor() {
+        this.nombre = 'Videojuegos 2018';
+        this.mejor_juego = 'GTAV';
+        this.mejor_juego_retro = 'Super mario 64';
+        this.mostrar_retro = true;
+        this.year = 2018;
+        this.color= this.mostrar_retro ? "red" : "yellow";
+        this.videojuegos = [
+            'los simpson hit and rin',
+            'assins creed',
+            'Dragon ball Z',
+            'Teken Tag',
+            'Naruto Duo'
+        ]
+    
+    }
 }
